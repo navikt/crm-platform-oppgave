c58261e5409bf05626d34975c3eaa0775691293c
SRS <stian.schikora@nav.no>
2022-05-05 08:58:28 +0200

    Handle redirect and tab close

--- force-app/main/default/aura/crmOppgaveRedirect/crmOppgaveRedirectController.js
+++ force-app/main/default/aura/crmOppgaveRedirect/crmOppgaveRedirectController.js
@@ -1,5 +1,29 @@
 ({
     init: function (cmp, event, helper) {
         helper.runFlow(cmp);
+    },
+
+    handleStatusChange: function (component, event) {
+        //Check if flow is finished
+        const status = event.getParam('status');
+
+        if (status === 'FINISHED' || status === 'FINISHED_SCREEN') {
+            let workspaceAPI = component.find('workspace');
+            workspaceAPI
+                .getEnclosingTabId()
+                .then((tabId) => {
+                    workspaceAPI
+                        .closeTab({ tabId: tabId })
+                        .then((response) => {
+                            //Success
+                        })
+                        .catch((error) => {
+                            console.log(JSON.stringify(error, null, 2));
+                        });
+                })
+                .catch((error) => {
+                    console.log(JSON.stringify(error, null, 2));
+                });
+        }
     }
 });
