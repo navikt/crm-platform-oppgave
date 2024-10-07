e78a74f7aa4548ea2118d3d29260e734e3a3d41e
Vegard Sørheller <vegardsorheller@Vegard-sin-MacBook-Pro.local>
2024-10-07 15:10:33 +0200

    Fixing review comments

--- force-app/main/default/lwc/crmNavTaskCommunalQuickText/crmNavTaskCommunalQuickText.js
+++ force-app/main/default/lwc/crmNavTaskCommunalQuickText/crmNavTaskCommunalQuickText.js
@@ -1,23 +1,33 @@
-import { LightningElement, api } from 'lwc';
-import hurtigtekster from '@salesforce/resourceUrl/NKS_NavTaskQuickText';
+import { LightningElement, api, wire } from 'lwc';
+import hurtigtekster from '@salesforce/resourceUrl/NKS_NavTaskCommunalQuickText';
 import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
+import crmSingleValueUpdate from '@salesforce/messageChannel/crmSingleValueUpdate__c';
 import addBusinessDays from '@salesforce/apex/CRM_DueDateUtility.addBusinessDays';
+import { subscribe, unsubscribe, MessageContext } from 'lightning/messageService';
+
+const communalTheme = 'KOM';
 
 export default class CrmNavTaskQuickText extends LightningElement {
     @api selectedValue;
+
     selectedValueLabel;
+    isCommunalTheme = false;
 
     options = [];
     items = [];
 
+    @wire(MessageContext)
+    messageContext;
+
     connectedCallback() {
+        this.subscribeToMessageChannel();
         if (this.items.length === 0) {
             fetch(hurtigtekster)
                 .then((response) => response.json())
                 .then((data) => {
                     this.items = data;
                     this.options = this.items.map((item) => {
-                        return { label: item.name, value: item.hurtigtekst };
+                        return { label: item.name, value: item.hurtigtekst, hasVirkedager: item.hasVirkedager };
                     });
                 })
                 .catch((error) => {
@@ -26,11 +36,34 @@ export default class CrmNavTaskQuickText extends LightningElement {
         }
     }
 
+    disconnectedCallback() {
+        this.unsubscribeToMessageChannel();
+    }
+
+    subscribeToMessageChannel() {
+        if (!this.subscription) {
+            this.subscription = subscribe(this.messageContext, crmSingleValueUpdate, (message) =>
+                this.handleMessage(message)
+            );
+        }
+    }
+
+    unsubscribeToMessageChannel() {
+        unsubscribe(this.subscription);
+        this.subscription = null;
+    }
+
+    handleMessage(message) {
+        if (message.name == 'themeCode') {
+            this.isCommunalTheme = message.value == communalTheme ? true : false;
+        }
+    }
+
     handleChange(event) {
         const selectedOption = this.options.find((opt) => opt.value === event.detail.value);
         this.displayedValue = selectedOption.label;
         this.selectedValue = selectedOption.value;
-        if (this.selectedValue?.includes('{TO_VIRKEDAGER_FRA_DAGENS_DATO}')) {
+        if (selectedOption?.hasVirkedager) {
             this.calculateAndReplaceBusinessDate(2);
         } else {
             this.dispatchAttributeChange();
@@ -40,7 +73,12 @@ export default class CrmNavTaskQuickText extends LightningElement {
     calculateAndReplaceBusinessDate(numberOfBusinessDays) {
         addBusinessDays({ numberofBusinessdays: numberOfBusinessDays })
             .then((result) => {
-                let formattedDate = this.formatDateDDMMYYYY(new Date(result));
+                const dateFromString = new Date(result);
+                let formattedDate = dateFromString.toLocaleDateString('no-NO', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit'
+                });
                 this.selectedValue = this.selectedValue.replace('{TO_VIRKEDAGER_FRA_DAGENS_DATO}', formattedDate);
                 this.dispatchAttributeChange();
             })
@@ -52,13 +90,5 @@ export default class CrmNavTaskQuickText extends LightningElement {
     dispatchAttributeChange() {
         const attributeChangeEvent = new FlowAttributeChangeEvent('selectedValue', this.selectedValue);
         this.dispatchEvent(attributeChangeEvent);
-    }
-
-    formatDateDDMMYYYY(date) {
-        let day = String(date.getDate()).padStart(2, '0');
-        let month = String(date.getMonth() + 1).padStart(2, '0');
-        let year = date.getFullYear();
-
-        return `${day}.${month}.${year}`;
     }
 }
