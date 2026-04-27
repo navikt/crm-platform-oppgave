31ecf6d94e67f09bf21fb24588436c33ed38515f
Vegard Sorheller <vegard.sorheller@nav.no>
2026-04-27 13:48:16 +0200

    Components for oppgave

--- force-app/main/default/lwc/crmOppgaveComments/crmOppgaveComments.js
+++ force-app/main/default/lwc/crmOppgaveComments/crmOppgaveComments.js
@@ -1,35 +1,25 @@
-import { LightningElement, api, wire } from 'lwc';
-import getOppgaveById from '@salesforce/apex/OppgaveManager.getOppgaveById';
+import { LightningElement, api } from 'lwc';
 
 export default class CrmOppgaveComments extends LightningElement {
-    @api oppgaveId;
+    @api kommentarer;
 
-    comment;
-    errorMessage;
-
-    @wire(getOppgaveById, { oppgaveId: '$oppgaveId' })
-    wiredComment({ data, error }) {
-        if (data) {
-            this.comment = (data.kommentarer ?? []).map((item) => ({
-                ...item,
-                opprettetTidspunktFormatted: item.opprettet?.tidspunkt
-                    ? new Date(item.opprettet.tidspunkt).toLocaleString('nb-NO', {
-                          day: '2-digit',
-                          month: '2-digit',
-                          year: 'numeric',
-                          hour: '2-digit',
-                          minute: '2-digit'
-                      })
-                    : ''
-            }));
-        }
-        if (error) {
-            console.error('Error in wiredComment:', JSON.stringify(error, null, 2));
-            this.errorMessage = error?.body?.message ?? JSON.stringify(error);
-        }
+    get formattedComments() {
+        return (this.kommentarer ?? []).map((item) => ({
+            tekst: item.tekst,
+            opprettetAv: item.opprettet?.av?.medarbeider?.navident ?? '',
+            opprettetTidspunktFormatted: item.opprettet?.tidspunkt
+                ? new Date(item.opprettet.tidspunkt).toLocaleString('nb-NO', {
+                      day: '2-digit',
+                      month: '2-digit',
+                      year: 'numeric',
+                      hour: '2-digit',
+                      minute: '2-digit'
+                  })
+                : ''
+        }));
     }
 
     get hasComments() {
-        return this.comment?.length > 0;
+        return this.kommentarer?.length > 0;
     }
 }
